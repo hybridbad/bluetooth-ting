2dee78824810b1bc2c90a8ed3b6b289f129a3ac0
James Davies <hybridbad90@gmail.com>
2019-08-19 17:05:13 +0100

    Fixes

--- App.js
+++ App.js
@@ -4,107 +4,103 @@ import { BleManager } from 'react-native-ble-plx';
 
 export default class App extends Component{
 
-    constructor(){
-        super();
-        this.manager = new BleManager();
-        this.devices = [];
-    }
+  constructor(){
+      super();
+      this.manager = new BleManager();
+      this.devices = [];
 
-    state = {
-      permissionStatus:'denied', 
-      bluetoothStatus: 'disabled',
-      info:''
+      this.state = {
+        permissionStatus:'denied', 
+        bluetoothStatus: 'disabled',
+        info:''
+    }
   }
 
-    info(message) {
-        this.setState({info: message})
-    }
+  info(message) {
+      this.setState({info: message})
+  }
 
-    error(message) {
-        this.setState({info: "ERROR: " + message})
-    }
+  error(message) {
+      this.setState({info: "ERROR: " + message})
+  }
 
-    async requestPermission() {
-        try {
-          const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+  async requestPermission() {
+      try {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
 
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            this.setState({permissionStatus:'granted'});
-          }else if(granted === PermissionsAndroid.RESULTS.DENIED) {
-            this.setState({permissionStatus:'denied'});
-          }else{
-             //console.log('never-ask-again');
-          }
-        } catch (err) {
-          console.error(err)
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          this.setState({permissionStatus:'granted'});
+        }else if(granted === PermissionsAndroid.RESULTS.DENIED) {
+          this.setState({permissionStatus:'denied'});
         }
+      } catch (err) {
+        console.error(err)
       }
-
-    scanAndConnect(){
-        this.manager.startDeviceScan(null, null, (error, device) => {
-          this.info("Scanning...")
-          this.devices.push(device.name)
-          if(error){
-            this.error(error.message)
-            return
-          }
-        })
-
-        
     }
 
-    startScan = () => {
-      this.info("Scanning....")
-      this.scanAndConnect();
-      this.devices = [];
-      this.timer();
-    }
+  scanAndConnect() {
+    this.devices = [];
 
-    stopScan = () => {
-      this.info("Scanner Stopped")
-      this.manager.stopDeviceScan();
-    }
+    this.manager.startDeviceScan(null, {allowDuplicates:false}, (error, device) => {
+      this.info("Scanning...")
+      this.devices.push(device.name)
+      if(error){
+        this.error(error.message)
+        return
+      }
+    })    
+  }
 
-    timer = () => {
-      setTimeout(() => {
-        this.stopScan();
-      }, 5000)
-    }
+  startScan() {
+    this.info("Scanning....")
+    this.scanAndConnect()
+    this.timer()
+  }
 
-    componentDidMount()
-    {
-        this.requestPermission();
-        const subscription = this.manager.onStateChange((state) => {
-            if(state === 'PoweredOn'){
-                this.setState({bluetoothStatus:'enabled'});
-                this.scanAndConnect();
-                subscription.remove();
-            } else{
-                this.setState({bluetoothStatus:'disabled'});
-            }
-        }, true);
+  stopScan() {
+    this.info("Scanner Stopped")
+    this.manager.stopDeviceScan();
+  }
 
-        this.timer();
-    }
+  timer() {
+    setTimeout(() => {
+      this.stopScan();
+    }, 7000)
+  }
 
-    render(){
-        return(
-             <ScrollView style={{flex:1, padding:15}}>
-                <Text style={{fontSize:20, alignSelf:'center', color:'steelblue'}}>
-                    Location access is {this.state.permissionStatus}
-                </Text>
-                <Text style={{fontSize:20, alignSelf:'center', color:'blue'}}>
-                    Bluetooth is {this.state.bluetoothStatus}
-                </Text>
-                <Text style={{fontSize:20, alignSelf:'center', color:'black'}}>
-                    {this.state.info}
-                </Text>
-                <Button style={{padding:5}} title="Start Scanning" onPress={this.startScan}><Text>Start Scanning</Text></Button>
-                <Button style={{padding:5}} title="Stop Scanning" onPress={this.stopScan}><Text>Stop Scanning</Text></Button>
-                <Text style={{fontSize:20, alignSelf:'center', color:'black'}}>
-                    {this.devices}
-                </Text>
-             </ScrollView>
-        );
-    }
+  componentDidMount() {
+    this.requestPermission();
+    const subscription = this.manager.onStateChange((state) => {
+      if(state === 'PoweredOn'){
+        this.setState({bluetoothStatus:'enabled'});
+        this.scanAndConnect();
+        subscription.remove();
+      } else {
+        this.setState({bluetoothStatus:'disabled'});
+      }
+    }, true);
+
+    this.timer();
+  }
+
+  render(){
+      return(
+            <ScrollView style={{flex:1, padding:15}}>
+              <Text style={{fontSize:20, alignSelf:'center', color:'steelblue'}}>
+                  Location access is {this.state.permissionStatus}
+              </Text>
+              <Text style={{fontSize:20, alignSelf:'center', color:'blue'}}>
+                  Bluetooth is {this.state.bluetoothStatus}
+              </Text>
+              <Text style={{fontSize:20, alignSelf:'center', color:'black'}}>
+                  {this.state.info}
+              </Text>
+              <Button style={{padding:5}} title="Start Scanning" onPress={this.startScan}><Text>Start Scanning</Text></Button>
+              <Button style={{padding:5}} title="Stop Scanning" onPress={this.stopScan}><Text>Stop Scanning</Text></Button>
+              <Text style={{fontSize:20, alignSelf:'center', color:'black'}}>
+                  {this.devices}
+              </Text>
+            </ScrollView>
+      );
+  }
 }
